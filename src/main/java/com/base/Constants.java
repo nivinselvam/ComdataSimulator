3c375ccda64115b6ac378d3048b46e8988c91618
Nivin Selvam P <NivinP1@verifone.com>
2024-12-05 21:30:55 +0530

    Logger implemented

---
+++ src/main/java/com/base/Constants.java
@@ -0,0 +1,23 @@
+package com.base;
+
+public class Constants {
+
+
+    /*
+    -----------------------------------Folder names-----------------------------------------
+     */
+    public static final String FOLDER_PROPERTY_FILES = "propertyFiles";
+    public static final String FOLDER_LOGS = "logs";
+
+    /*
+    ----------------------------------- File names  -----------------------------------------
+     */
+    public static final String FILE_LOG_PROPERTIES = "log4j2.xml";
+    public static final String FILE_APPLICATION_LOG = "messages.log";
+    public static final String FILE_SIMLATOR_PROPERTIES = "simulatorConfig.json";
+
+    /*
+    -----------------------------------    Path     -----------------------------------------
+     */
+    public static final String PATH_LOG_PROPERTIES_FILE = FOLDER_PROPERTY_FILES + "/" + FILE_LOG_PROPERTIES;
+}
--- src/main/java/com/base/Main.java
+++ src/main/java/com/base/Main.java
@@ -1,4 +1,15 @@
 package com.base;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+    public static final Logger logger = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+
+    }
+
+
 }
